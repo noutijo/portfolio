81c1f06fd18127054981edebb22357d126ddfd8d
Oreol Noumodong <oreolnoumodong@gmail.com>
2023-06-22 23:32:06 +0100

    refactor

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    //required:
-    appDir: true,
-    fontLoaders: [
-      { loader: "@next/font/google", options: { subsets: ["latin"] } },
-    ],
-  },
-};
+  swcMinify: true
+}
 
 module.exports = nextConfig
